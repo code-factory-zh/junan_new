5b109dac4f98de35eb2311ef9661316a902f5fa0
qiuxiangcheng <342527858@qq.com>
2019-04-10 23:33:39 +0800

    wxpay

--- Application/Buy/Controller/OrderController.class.php
+++ Application/Buy/Controller/OrderController.class.php
@@ -66,21 +66,23 @@ class OrderController extends CommonController{
 		$openId = $tools -> GetOpenid();
 
 		$input = new \WxPayUnifiedOrder();
-		$input->SetBody("test");
-		$input->SetAttach("test");
-		$input->SetOut_trade_no("sdkphp".date("YmdHis"));
-		$input->SetTotal_fee("1");
-		$input->SetTime_start(date("YmdHis"));
-		$input->SetTime_expire(date("YmdHis", time() + 600));
-		$input->SetGoods_tag("test");
-		$input->SetNotify_url("http://wxpay.joinersafe.com/manage/pay/setpay");
-		$input->SetTrade_type("JSAPI");
-		$input->SetOpenid($openId);
+		$input -> SetBody("test");
+		$input -> SetAttach("test");
+		$input -> SetOut_trade_no("sdkphp".date("YmdHis"));
+		$input -> SetTotal_fee("1");
+		$input -> SetTime_start(date("YmdHis"));
+		$input -> SetTime_expire(date("YmdHis", time() + 600));
+		$input -> SetGoods_tag("test");
+		$input -> SetNotify_url("http://wxpay.joinersafe.com/manage/pay/setpay");
+		$input -> SetTrade_type("JSAPI");
+		$input -> SetOpenid($openId);
 		$config = new \WxPayConfig();
 		$order = \WxPayApi::unifiedOrder($config, $input);
 
-		pr($input);
+		$jsApiParameters = $tools -> GetJsApiParameters($order);
 
+		$editAddress = $tools -> GetEditAddressParameters();
+		pr($editAddress);
 
 		$this -> rel(['self_order_sn' => $orderNum, 'price' => floatval($p['total_price'])]) -> e();
 	}
