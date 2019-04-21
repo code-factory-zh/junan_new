cf346e6bb072ff4241eb2b3ff8d6762c5079c4eb
cui <cuiruijunphp@163.com>
2019-04-22 00:41:36 +0800

    下载调试

--- Application/Manage/Controller/QuestionController.class.php
+++ Application/Manage/Controller/QuestionController.class.php
@@ -315,28 +315,35 @@ class QuestionController extends CommonController
 	public function download()
 	{
 		$file_path = $_SERVER['DOCUMENT_ROOT']."download/demo.xlsx";
+		if(!file_exists($file_path)){
+			echo '地址不存在';
+			die();
+		}
 
 		$file_name = "demo.xlsx";
 
-		$fp=fopen($file_path,"r+");
-		$file_size=filesize($file_path);
-		//下载文件需要用到的头
-		Header("Content-type: application/octet-stream");
-		header("Content-Transfer-Encoding: binary");
-		Header("Accept-Ranges: bytes");
-		Header("Accept-Length:".$file_size);
-		Header("Content-Disposition: attachment; filename=".$file_name);
-		ob_clean();
-		flush();
-		$buffer=1024;
-		$file_count=0;
-		//向浏览器返回数据
-		while(!feof($fp) && $file_count<$file_size){
-			$file_con=fread($fp,$buffer);
-			$file_count+=$buffer;
-			echo $file_con;
+		if(false !== ($fp=fopen($file_path,"r+"))){
+			$file_size=filesize($file_path);
+			//下载文件需要用到的头
+			Header("Content-type: application/octet-stream");
+			header("Content-Transfer-Encoding: binary");
+			Header("Accept-Ranges: bytes");
+			Header("Accept-Length:".$file_size);
+			Header("Content-Disposition: attachment; filename=".$file_name);
+			ob_clean();
+			flush();
+			$buffer=1024;
+			$file_count=0;
+			//向浏览器返回数据
+			while(!feof($fp) && $file_count<$file_size){
+				$file_con=fread($fp,$buffer);
+				$file_count+=$buffer;
+				echo $file_con;
+			}
+			fclose($fp);
+		}else{
+			echo '读取文件失败';
 		}
-		fclose($fp);
 	}
 
 	/**
